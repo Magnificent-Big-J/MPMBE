8d8f4cf2cd6259924910b82912187ef9287b9da7
Magnificent-Big-J <joel.mnisi@protonmail.com>
2019-07-16 17:24:54 +0200

    Changing login screen

--- resources/js/components/views/Login.vue
+++ resources/js/components/views/Login.vue
@@ -1,5 +1,5 @@
 <template>
-    <div class="mt-5">
+    <!--div class="mt-5">
 
         <v-content>
             <v-container fluid fill-height>
@@ -33,7 +33,43 @@
                 </v-layout>
             </v-container>
         </v-content>
-    </div>
+    </div-->
+    <v-app id="login" class="primary">
+        <v-content>
+            <v-container fluid fill-height>
+                <v-layout align-center justify-center>
+                    <v-flex xs12 sm8 md4 lg4>
+                        <v-card class="elevation-1 pa-3">
+                            <v-card-text>
+                                <div class="layout column align-center">
+
+                                    <h1 class="flex my-4 primary--text">MPMBE Log In</h1>
+                                </div>
+
+                                <v-form >
+                                    <v-alert v-if="error"
+                                             :value="true"
+                                             type="error"
+                                    >
+                                        Invalid Credentials
+                                    </v-alert>
+
+                                    <v-text-field  prepend-icon="person" required  v-model="form.email"  type="email" label="Email" ></v-text-field>
+                                    <v-text-field prepend-icon="lock" required  v-model="form.password" label="Password" id="password" type="password"></v-text-field>
+                                </v-form>
+                            </v-card-text>
+                            <v-card-actions>
+                                <v-btn color="primary" @click="login">Login</v-btn>
+                                <v-spacer></v-spacer>
+
+                            </v-card-actions>
+                        </v-card>
+                    </v-flex>
+                </v-layout>
+            </v-container>
+
+        </v-content>
+    </v-app>
 </template>
 
 <script>
@@ -67,5 +103,16 @@
 </script>
 
 <style scoped>
-
+    #login {
+        height: 50%;
+        width: 100%;
+        position: absolute;
+        top: 0;
+        left: 0;
+        content: "";
+        z-index: 0;
+    }
+    .link-style {
+        text-decoration: none;
+    }
 </style>
